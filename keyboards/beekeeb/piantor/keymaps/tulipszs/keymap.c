6a37fa0276249b49e2db09cc84b2ccd6f35ab550
Anthony DeLorenzo <anthony@crystalpeaksecurity.com>
2026-03-16 08:35:44 -0400

    Update keymap

--- keyboards/beekeeb/piantor/keymaps/tulipszs/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/tulipszs/keymap.c
@@ -114,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             //|--------+--------+--------+--------+--------+--------|                                       |--------+--------+--------+--------+--------+--------|
 MT(MOD_LSFT, KC_MINUS),     KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                               KC_H,   KC_J,   KC_K,   KC_L,   KC_SEMICOLON,  KC_QUOT,
             //|--------+--------+--------+--------+--------+--------|                                       |--------+--------+--------+--------+--------+--------|
-        MT(MOD_LCTL, KC_EQL),    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                                               KC_N,   KC_M,   KC_COMM, KC_DOT,    KC_SLSH,  KC_BSLS,
+        MT(MOD_LCTL, KC_DEL),    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,                                               KC_N,   KC_M,   KC_COMM, KC_DOT,    KC_SLSH,  KC_BSLS,
             //|--------+--------+--------+--------+--------+--------+--------|                      |--------+--------+--------+--------+--------+--------+--------|
                         MT(MOD_LALT, KC_TAB) , LT(_RAISE, KC_BSPC), MT(MOD_LSFT, KC_EQL),           MT(MOD_LCTL, KC_ENT), LT(_LOWER, KC_SPC), MT(MOD_LALT, KC_DEL)
             //`--------------------------'  `--------------------------'
